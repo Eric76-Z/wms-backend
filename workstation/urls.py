f62278f3cfe825e38b90d6c768771e8f74189c2c
hasaii1 <hasaii1@outlook.com>
2021-09-01 18:25:21 +0800

    忘记密码模块

--- workstation/urls.py
+++ workstation/urls.py
@@ -11,12 +11,14 @@ from rest_framework.routers import SimpleRouter, DefaultRouter
 router = SimpleRouter()
 
 # 2,注册视图集
-router.register('location', views.LocationsViewset, basename='location')
+router.register('location', views.LocationsViewSet, basename='location')
 router.register('parts', views.PartsViewSet, basename='parts')
 router.register('bladeitem', views.BladeItemViewSet, basename='bladeitem')
 router.register('images', views.ImagesViewSet, basename='images')
 router.register('weldinggun', views.WeldingGunViewSet, basename='weldinggun')
 router.register('maintenance', views.MaintenanceRecordsViewSet, basename='maintenance')
+router.register('sort', views.SortViewSet, basename='sort')
+
 
 urlpatterns += router.urls
 
